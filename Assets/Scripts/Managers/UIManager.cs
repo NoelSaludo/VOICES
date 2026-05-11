54b570c58c2118a250696d88d58f55bdb63591cb
noel zyrence saludo <noelsaludo17@gmail.com>
2026-05-11 15:38:04 +0800

    Fixed collisions in Level1 scene and locked player z axis rotation

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -10,7 +10,7 @@ public class UIManager : MonoBehaviour
     {
         if (instance == null)
         {
-            instance = FindObjectOfType<UIManager>();
+            instance = GameObject.Find("UI Manager").GetComponent<UIManager>();
         }
 
         return instance;
